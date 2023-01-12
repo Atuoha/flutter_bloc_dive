1e4af4527d7d30de41b2f90ce5e766f66a6cd887
Atuoha <atuohainitiatives@gmail.com>
2023-01-12 19:10:31 +0100

    working on cubit

---
+++ android/app/src/main/kotlin/com/example/diving_into_flutter_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diving_into_flutter_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math show Random;
+import 'package:bloc/bloc.dart';
+
+final List<String> names = ['Adam', 'Jacob', 'Henry'];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void pickRandomName() => emit(names.getRandomElement());
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final NamesCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cubit.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('BLoC')),
+      body: StreamBuilder<String?>(
+        stream: cubit.stream,
+        builder: (context, snapshot) {
+          final button = TextButton(
+            onPressed: () => cubit.pickRandomName(),
+            child: const Text('Pick a random name'),
+          );
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return button;
+            case ConnectionState.waiting:
+              return button;
+            case ConnectionState.active:
+              return Column(
+                children: [
+                  Text(snapshot.data! ?? ''),
+                  const SizedBox(height: 10),
+                  button,
+                ],
+              );
+            case ConnectionState.done:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
+  }
+}
